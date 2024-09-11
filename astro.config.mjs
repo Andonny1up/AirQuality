dc3bbaeadaf0be96e94d7c37d994cadd74ef992a
Andoni1Barba <Andoni1barba@gmail.com>
2024-09-11 09:38:26 -0400

    astro install adapter node

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,14 @@ import tailwind from '@astrojs/tailwind';
 
 import react from '@astrojs/react';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   integrations: [tailwind(), react()],
+
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
